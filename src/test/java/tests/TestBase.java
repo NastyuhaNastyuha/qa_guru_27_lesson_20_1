d4d6ea2f9c76d7eb6ece321024e73db93ca02501
Nastya Rybina <przh.17@gmail.com>
2024-08-28 22:56:57 +0600

    config fixes

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -5,6 +5,7 @@ import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import config.LocalMobileDriverReader;
 import drivers.BrowserstackDriver;
+import drivers.LocalMobileDriver;
 import helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterAll;
@@ -21,9 +22,16 @@ public class TestBase {
 //        Configuration.browser = BrowserstackDriver.class.getName();
 //        Configuration.browserSize = null;
 //        Configuration.timeout = 10000;
-        LocalMobileDriverReader localMobileDriverReader = new LocalMobileDriverReader();
-        localMobileDriverReader.setLocalMobileDriverConfig();
-    }
+//        LocalMobileDriverReader localMobileDriverReader = new LocalMobileDriverReader();
+//        localMobileDriverReader.setLocalMobileDriverConfig();
+            Configuration.browserSize = null;
+            if (System.getProperty("deviceHost", "browserstack").equals("emulation")) {
+                Configuration.browser = LocalMobileDriver.class.getName();
+            } else if (System.getProperty("deviceHost", "browserstack").equals("browserstack")) {
+                Configuration.browser = BrowserstackDriver.class.getName();
+            }
+        }
+
 
     @BeforeEach
     void beforeEach() {
